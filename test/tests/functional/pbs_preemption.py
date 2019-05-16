d5381074830eedd0eebeb7aeab4d68ac2de9efbc
Arun Grover <arun.grover@altair.com>
2019-05-16 10:26:00 -0700

    Fixed when high priority requesting vnode fails to preempt (#1114)

--- test/tests/functional/pbs_preemption.py
+++ test/tests/functional/pbs_preemption.py
@@ -55,6 +55,14 @@ class TestPreemption(TestFunctional):
              'enabled': 'True',
              'Priority': 200}
         self.server.manager(MGR_CMD_CREATE, QUEUE, a, "expressq")
+        if len(self.moms) == 2:
+            self.mom1 = self.moms.keys()[0]
+            self.mom2 = self.moms.keys()[1]
+            # Since some tests need multi-node setup and majority don't,
+            # delete the second node so that single node tests don't fail.
+            # Tests needing multi-node setup will create the second node
+            # explicity.
+            self.server.manager(MGR_CMD_DELETE, NODE, id=self.mom2)
 
     def submit_and_preempt_jobs(self, preempt_order='R'):
         """
@@ -126,3 +134,190 @@ exit 0
         marked as "Job will never run"
         """
         self.submit_and_preempt_jobs(preempt_order='R')
+
+
+    def test_qalter_preempt_targets_to_none(self):
+        """
+        Test that a job requesting preempt targets set to two different queues
+        can be altered to set preempt_targets as NONE
+        """
+
+        # create an addition queue
+        a = {'queue_type': 'execution',
+             'started': 'True',
+             'enabled': 'True'}
+        self.server.manager(MGR_CMD_CREATE, QUEUE, a, "workq2")
+
+        self.server.manager(MGR_CMD_SET, SCHED, {'scheduling': 'False'})
+        # submit a job in expressq with preempt targets set to workq, workq2
+        a = {'Resource_List.preempt_targets': 'queue=workq,queue=workq2'}
+        j = Job(TEST_USER, a)
+        jid = self.server.submit(j)
+
+        self.server.alterjob(jobid=jid,
+                             attrib={'Resource_List.preempt_targets': 'None'})
+        self.server.expect(JOB, id=jid,
+                           attrib={'Resource_List.preempt_targets': 'None'})
+
+    def test_preempt_sort_when_set(self):
+        """
+        This test is for preempt_sort when it is set to min_time_since_start
+        """
+        a = {ATTR_rescavail + '.ncpus': 2}
+        self.server.manager(MGR_CMD_SET, NODE, a, self.mom.shortname)
+
+        a = {'preempt_sort': 'min_time_since_start'}
+        self.server.manager(MGR_CMD_SET, SCHED, a)
+        jid1, jid2, jid3 = self.submit_jobs()
+        self.server.expect(JOB, {ATTR_state: 'R'}, id=jid1)
+        self.server.expect(JOB, {ATTR_state: 'S'}, id=jid2)
+        self.server.expect(JOB, {ATTR_state: 'R'}, id=jid3)
+
+    def test_preempt_sort_when_unset(self):
+        """
+        This test is for preempt_sort when it is unset
+        """
+        a = {ATTR_rescavail + '.ncpus': 2}
+        self.server.manager(MGR_CMD_SET, NODE, a, self.mom.shortname)
+
+        self.server.manager(MGR_CMD_UNSET, SCHED, 'preempt_sort')
+        jid1, jid2, jid3 = self.submit_jobs()
+        self.server.expect(JOB, {ATTR_state: 'S'}, id=jid1)
+        self.server.expect(JOB, {ATTR_state: 'R'}, id=jid2)
+        self.server.expect(JOB, {ATTR_state: 'R'}, id=jid3)
+
+    def test_preempt_retry(self):
+        """
+        Test to make sure that preemption is retried if it fails.
+        """
+        a = {'resources_available.ncpus': 2}
+        self.server.manager(MGR_CMD_SET, NODE, a, id=self.mom.shortname)
+
+        abort_script = """#!/bin/bash
+exit 3
+"""
+        abort_file = self.du.create_temp_file(body=abort_script)
+        self.du.chmod(path=abort_file, mode=0755)
+        self.du.chown(path=abort_file, uid=0, gid=0, runas=ROOT_USER)
+        c = {'$action': 'checkpoint_abort 30 !' + abort_file}
+        self.mom.add_config(c)
+
+        # submit two jobs to regular queue
+        j1 = Job(TEST_USER)
+        jid1 = self.server.submit(j1)
+
+        time.sleep(2)
+
+        j2 = Job(TEST_USER)
+        jid2 = self.server.submit(j2)
+
+        self.server.expect(JOB, {'job_state': 'R'}, id=jid2)
+        self.server.expect(JOB, {'job_state': 'R'}, id=jid2)
+
+        # set preempt order
+        self.server.manager(MGR_CMD_SET, SCHED, {'preempt_order': 'C'})
+
+        # submit a job to high priority queue
+        a = {ATTR_q: 'expressq'}
+        j3 = Job(TEST_USER, a)
+        jid3 = self.server.submit(j3)
+        self.server.expect(JOB, {'job_state': 'R'}, id=jid1)
+        self.server.expect(JOB, {'job_state': 'R'}, id=jid2)
+        self.server.expect(JOB, {'job_state': 'Q'}, id=jid3)
+
+        self.mom.log_match(jid2 + ";checkpoint failed:")
+        self.mom.log_match(jid1 + ";checkpoint failed:")
+
+        abort_script = """#!/bin/bash
+kill -9 $1
+exit 0
+"""
+        abort_file = self.du.create_temp_file(body=abort_script)
+        self.du.chmod(path=abort_file, mode=0755)
+        self.du.chown(path=abort_file, uid=0, gid=0, runas=ROOT_USER)
+        c = {'$action': 'checkpoint_abort 30 !' + abort_file + ' %sid'}
+        self.mom.add_config(c)
+
+        self.server.manager(MGR_CMD_SET, SERVER, {'scheduling': 'True'})
+        self.server.expect(JOB, {'job_state': 'R'}, id=jid1)
+        self.server.expect(JOB, {'job_state': 'Q'}, id=jid2)
+        self.server.expect(JOB, {'job_state': 'R'}, id=jid3)
+
+    def test_vnode_resource_contention(self):
+        """
+        Test to make sure that preemption happens when the resource in
+        contention is vnode.
+        """
+
+        a = {'resources_available.ncpus': 2}
+        self.server.create_vnodes(name='vnode', attrib=a, num=11,
+                                  mom=self.mom, usenatvnode=False)
+
+        a = {'Resource_List.select': '1:ncpus=2+1:ncpus=2'}
+        for _ in range(5):
+            j = Job(TEST_USER, attrs=a)
+            jid = self.server.submit(j)
+            self.server.expect(JOB, {'job_state': 'R'}, id=jid)
+
+        # Randomly select a vnode with running jobs on it. Request this
+        # vnode in the high priority job later.
+        self.server.expect(NODE, {'state': 'job-busy'}, id='vnode[4]')
+
+        a = {ATTR_q: 'expressq', 'Resource_List.vnode': 'vnode[4]'}
+        hj = Job(TEST_USER, attrs=a)
+        hjid = self.server.submit(hj)
+        self.server.expect(JOB, {'job_state': 'R'}, id=hjid)
+
+        # Since high priority job consumed only one ncpu, vnode[4]'s
+        # node state should be free now
+        self.server.expect(NODE, {'state': 'free'}, id='vnode[4]')
+
+    def test_host_resource_contention(self):
+        """
+        Test to make sure that preemption happens when the resource in
+        contention is host.
+        """
+        # Skip test if number of mom provided is not equal to two
+        if len(self.moms) != 2:
+            self.skipTest("test requires two MoMs as input, " +
+                          "use -p moms=<mom1>:<mom2>")
+        else:
+            self.server.manager(MGR_CMD_CREATE, NODE, id=self.mom2)
+
+        a = {'resources_available.ncpus': 2}
+        self.server.manager(MGR_CMD_SET, NODE, a, id=self.mom1)
+        a = {'resources_available.ncpus': 3}
+        self.server.manager(MGR_CMD_SET, NODE, a, id=self.mom2)
+
+        a = {'Resource_List.select': '1:ncpus=2'}
+        j1 = Job(TEST_USER, attrs=a)
+        jid1 = self.server.submit(j1)
+        j2 = Job(TEST_USER, attrs=a)
+        jid2 = self.server.submit(j2)
+        self.server.expect(JOB, {'job_state': 'R'}, id=jid1)
+        self.server.expect(JOB, {'job_state': 'R'}, id=jid2)
+
+        # Stat job to check which job is running on mom1
+        pjid = jid2
+        job_stat = self.server.status(JOB, id=jid1)
+        ehost = job_stat[0]['exec_host'].partition('/')[0]
+        if ehost == self.mom1:
+            pjid = jid1
+
+        # Submit a express queue job requesting the host
+        a = {ATTR_q: 'expressq', 'Resource_List.host': self.mom1}
+        hj = Job(TEST_USER, attrs=a)
+        hjid = self.server.submit(hj)
+        self.server.expect(JOB, {'job_state': 'R'}, id=hjid)
+        self.server.expect(JOB, {'job_state': 'S'}, id=pjid)
+
+        # Submit another express queue job requesting the host,
+        # this job will stay queued
+        a = {ATTR_q: 'expressq', 'Resource_List.host': self.mom1,
+             'Resource_List.ncpus': 2}
+        hj2 = Job(TEST_USER, attrs=a)
+        hjid2 = self.server.submit(hj2)
+        self.server.expect(JOB, {'job_state': 'Q'}, id=hjid2)
+        comment = "Not Running: Insufficient amount of resource: host"
+        self.server.expect(JOB, {'comment': comment}, id=hjid2)
+
