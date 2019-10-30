37372686c2c61628911a7104b6d08d324d78f01c
Arun Grover <arun.grover@altair.com>
2019-10-30 13:04:18 -0700

    Fix for sched crash with entity limits (#1377)

--- src/scheduler/check.c
+++ src/scheduler/check.c
@@ -1338,10 +1338,9 @@ find_counts_elm(counts *cts_list, char *name, resdef *rdef, counts **cnt, resour
 			*cnt = cts;
 		if (rdef == NULL)
 			return cts->running;
-		else {
-			if ((res_lim = find_resource_count(cts->rescts, rdef)) != NULL)
-				if (rcount != NULL)
-					*rcount = res_lim;
+		else if ((res_lim = find_resource_count(cts->rescts, rdef)) != NULL) {
+			if (rcount != NULL)
+				*rcount = res_lim;
 			return res_lim->amount;
 		}
 	}
