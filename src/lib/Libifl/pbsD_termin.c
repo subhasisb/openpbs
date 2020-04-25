cf9acc83044f14bfabd93ce0d8232a7997d01eaa
bremanandjk <bremanandjk@gmail.com>
2020-04-25 15:54:32 -0400

    fix for code std

--- src/lib/Libifl/pbsD_termin.c
+++ src/lib/Libifl/pbsD_termin.c
@@ -86,9 +86,9 @@ send_terminate(int c, int sock, int manner, char *extend)
 
 	reply = PBSD_rdrpy_sock(sock, &rc);
 
-	if (reply == NULL) {
+	if (reply == NULL)
 		return pbs_errno;
-	}
+
 
 	if (set_conn_errno(c, reply->brp_code) != 0) {
 		pbs_errno = reply->brp_code;
